3ebebe5776e774d66277d86f5fd0d193dcc7a430
yagray <yagray@mail.ru>
2019-10-17 14:11:19 +0300

    ok

--- app/src/main/java/k/s/yarlykov/stylesapp/CustomDrawable.kt
+++ app/src/main/java/k/s/yarlykov/stylesapp/CustomDrawable.kt
@@ -13,11 +13,9 @@ package k.s.yarlykov.stylesapp
 import android.graphics.*
 import android.graphics.Paint.ANTI_ALIAS_FLAG
 import android.graphics.drawable.Drawable
-import android.util.Log
 import android.util.Property
-import androidx.core.content.ContextCompat
 
-class CustomDrawable(private val radius : Float = 64f) : Drawable() {
+class CustomDrawable(private val radius: Float = 64f) : Drawable() {
 
     var progress = 1f
         set(value) {
@@ -47,36 +45,54 @@ class CustomDrawable(private val radius : Float = 64f) : Drawable() {
 
     override fun draw(canvas: Canvas) {
         if (progress < 1f) {
-
-//            Log.e("APP_TAG", "progress=$progress")
-
-//            val progressEffect = DashPathEffect(
-//                floatArrayOf(0f, (1f - progress) * length, progress * length, 0f),
-//                initialPhase)
-
-            val progressEffect = DashPathEffect(
-                floatArrayOf(4f, (1f - progress) * 14f, 4f, (1f - progress) * 14f),
-                initialPhase)
+            val progressEffect = dashEffectCircleDrawing()
             linePaint.pathEffect = ComposePathEffect(progressEffect, cornerEffect)
-
         }
-        canvas.drawPath(path, linePaint)    }
+        canvas.drawPath(path, linePaint)
+    }
 
+    /**
+     * floatArrayOf(...) определяет вид пуктирной линии, которая будет рисовать path,
+     * а именно это набор из "тире" и "пробелов", точнее из длин (в пикселях) "тире" и "пробелов".
+     * Например floatArrayOf(5f, 10f, 20f, 30f) говорит, что сначала идет тире длиной 5,
+     * потом пробел длиной 10, потом снова тире длиной 20 и пробел длиной 30. Потом все циклино
+     * повторится по всей длине path. В указанном примере получится статическая картинка. Однако
+     * если реагировать на progress, то можно анимировать процесс отрисовки.
+     */
+    private fun dashEffectDashedDrawing(dash : Float = 16f) : DashPathEffect =
+        DashPathEffect(
+            floatArrayOf(dash, (1f - progress) * dash, dash, (1f - progress) * dash),
+            initialPhase
+        )
+
+    /**
+     * В этом алгоритме мы устанавливаем исходное состояние на момент начала
+     * рисования таким образом, что длина тире нулевая, а длина пробела -
+     * вся длина нашего path. По мере изменения progress от 0.00 до 1.00
+     * у нас будет уменьшаться длина пробела в первой паре, и увеличиваться
+     * длина тире во второй паре аргументов floatArrayOf. А значит мы будем
+     * увидим как последовательно отрисовываться линия окружности.
+     */
+    private fun dashEffectCircleDrawing() : DashPathEffect =
+        DashPathEffect(
+            floatArrayOf(0f, (1f - progress) * length, progress * length, 0f),
+            initialPhase
+        )
+
+    /**
+     * Переопределенные функции класса Drawable
+     */
     override fun setAlpha(alpha: Int) {
         linePaint.alpha = alpha
     }
-
     override fun getOpacity(): Int = PixelFormat.TRANSLUCENT
-
     override fun setColorFilter(colorFilter: ColorFilter?) {
         linePaint.colorFilter = colorFilter
     }
     override fun getIntrinsicWidth() = width
-
     override fun getIntrinsicHeight() = height
 
     companion object {
-
         private const val width = 256
         private const val height = 256
         private const val cx = (width / 2).toFloat()
@@ -87,7 +103,8 @@ class CustomDrawable(private val radius : Float = 64f) : Drawable() {
             override fun set(cd: CustomDrawable, value: Float) {
                 cd.progress = value
             }
-            override fun get(cd : CustomDrawable): Float = cd.progress
+
+            override fun get(cd: CustomDrawable): Float = cd.progress
         }
     }
 }
